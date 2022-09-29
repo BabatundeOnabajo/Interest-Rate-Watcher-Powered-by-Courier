8c4641bc94272e77d42af977b6b9be263d11e06e
Babatunde Valentine Onabajo <36359216+BabatundeOnabajo@users.noreply.github.com>
2022-09-29 13:51:59 +0100

    Update send.php

--- send.php
+++ send.php
@@ -1,7 +1,5 @@
 <?php
 
-# This file controls the actual sending of messages to end users about an interest rate change, with the help of Courier.
-
 $emailAddress = "INCLUDE_YOUR_EMAIL_ADDRESS_HERE"; #You should include your email address here. This email address will be used to notify you when the interest rate of the relevant central bank changes.
 
 # $emailAddress = $_POST['INCLUDE_NAME_OF_VARIABLE_FOR_EMAIL_ADDRESS_HERE']; #This line of code has been commented out. This can be uncommented out if you are intending to use this on a website of your own where users can enter their email address. This will likely be of interest to financial institutions, statistical authorities, and so on. 
@@ -97,14 +95,73 @@ $arrayOfInterestRates = array(); #We create an array of interest rates. Unlike w
 if(preg_match_all('/(\d+|\d+[.,]\d{1,4})(?=\s*%)/', $outputOfURLScrape, $matches)){ #Here we use a regular expression to extract the various percentages of the interest rates set by the central bank. 
   foreach($matches as $match){
       
-      $arrayOfInterestRates = array(); #We use a foreach(){} loop to iterate over the range of interest rates extracted from the website. In PHP, we can be reasonably sure that the order will remain the same so we do not need to be worried about the order changing. We will use this array to then send the appropriate email.
+      $arrayOfInterestRates = array(); #We use a foreach(){} loop to iterate over the range of interest rates extracted from the website. In PHP, we can be reasonably sure that the order will remain the same so we do not need to be worried about the order changing. This is helpful because it means that the interest rate of the central ban We will use this array to then send the appropriate email.
   }
     
 }
-       
-#We perform an if(){} statement here just to make sure that the appropriate databases and tables are created.
+
+# This is a list of interest rates for the countries this script covers.
+
+$interestRateOfTheUSA = $arrayOfInterestRates[70];
+
+foreach($arrayOfInterestRates as $particularInterestRate){ #We use this foreach(){} loop to iterate through the array and ensure that it is actually a number so that it is safe to include in the database.
+    
+    if(!is_numeric($particularInterestRate)){
+        unset($arrayOfInterestRates); #If there is *any* particular value that is not a number, as gauged by is_numeric function, then we unset the entire array.
+    }
+    
+}
 
 
+#We use this segment to scan the database and to check to see if the interest rate has changed.
+
+#This segment relates to the Federal Reserve in the United States. Remember, the US has the ID of 70 in the array identified as arrayOfInterestRates.
+$queryToSelectInterestRateOfUSA = "SELECT unique_id_of_data_relating_to_interest_rate, 	id_of_central_bank, interest_rate_set_by_central_bank, MAX(date_and_time_of_interest_rate_change) AS most_recent_date FROM data_relating_to_interest_rates_and_central_banks GROUP BY most_recent_date LIMIT 1"; #This is a query that selects the most recent date for the history of interest rates logged in the database for the Federal Reserve. This query should only return one at most inherently, and we have further ensured that by using a limit as well. 
+
+$resultOfQueryToSelectInterestRateOfUSA = mysqli_query($conn, $queryToSelectInterestRateOfUSA);
+
+while($row = mysqli_fetch_array($resultOfQueryToSelectInterestRateOfUSA, MYSQLI_ASSOC)){ #Here we analyse the database. 
+
+    if($row['interest_rate_set_by_central_bank'] > $interestRateOfTheUSA){ #If the interest rate set by the central bank for the USA (Fed. Reserve) in our database is less than that from which we have obtained on the website, we 1) insert a new entry into the database together with the date today 2) use Courier to inform the user that interest rates have changed. Note, however, that we only inform the end user if we think they are from the US. If not, then we need to proceed to a different if(){} statement for a different country.
+        
+$queryToInsertNewInterestRateOfUSA = "INSERT INTO interest_rate_watcher.data_relating_to_interest_rates_and_central_banks(id_of_central_bank, interest_rate_set_by_central_bank, date_and_time_of_interest_rate_change) VALUES" . " " . "(1," . $arrayOfInterestRates[70] . "," . date("Y-m-d") . ")";         
+        
+$resultOfQueryToInsertNewInterestRateOfUSA = mysqli_query($conn, $queryToInsertNewInterestRateOfUSA); #This inserts the interest rate into the database. 
+
+# This is where we send the mail to the user. We make the assumption, admittedly bold, that if the user is using a ".com" email address then they live in the US. 
+
+$response = $client->request('POST', 'https://api.courier.com/send', [
+  'body' => '{"message":{"content":{"title":"Interest Rates have risen - Federal Reserve","body":"Dear {INSERT_NAME_HERE}, interest rates by the Federal Reserve have now risen. This will likely increase the cost of borrowing for you."},"to":{"email":"' . $emailAddress . '","user_id":"Interest rates - Sep 2022","phone_number":"+447429601103"}}}',
+  'headers' => [
+    'Accept' => 'application/json',
+    'Authorization' => 'Bearer [INSERT_YOUR_AUTHORISATION_HERE]',
+    'Content-Type' => 'application/json',
+  ],
+]);
+
+# echo $response->getBody(); If you would like to see the JSON that has been returned, you can uncomment this.
+        
+    }elseif($row['interest_rate_set_by_central_bank'] < $interestRateOfTheUSA){
+        
+$queryToInsertNewInterestRateOfUSA = "INSERT INTO interest_rate_watcher.data_relating_to_interest_rates_and_central_banks(id_of_central_bank, interest_rate_set_by_central_bank, date_and_time_of_interest_rate_change) VALUES" . " " . "(1," . $arrayOfInterestRates[70] . "," . date("Y-m-d") . ")"; 
+        
+$resultOfQueryToInsertNewInterestRateOfUSA = mysqli_query($conn, $queryToInsertNewInterestRateOfUSA); #This inserts the interest rate into the database. 
+
+# This is where we send the mail to the user. We make the assumption, admittedly bold, that if the user is using a ".com" email address then they live in the US. 
+
+$response = $client->request('POST', 'https://api.courier.com/send', [
+  'body' => '{"message":{"content":{"title":"Interest Rates have risen - Federal Reserve","body":"Dear {INSERT_NAME_HERE}, interest rates by the Federal Reserve have now declined. This will likely decrease the cost of borrowing for you. Be sure to check with any lenders whether the interest rate change has affected you."},"to":{"email":"' . $emailAddress . '","user_id":"Interest rates - Sep 2022","phone_number":"+447429601103"}}}',
+  'headers' => [
+    'Accept' => 'application/json',
+    'Authorization' => 'Bearer [INSERT_YOUR_AUTHORISATION_HERE]',
+    'Content-Type' => 'application/json',
+  ],
+]);
+
+    }
+    
+    
+}
 
 ?>
 
