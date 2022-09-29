a5fc48554e5e5819eabac73b4b1791e84d981dc9
Babatunde Valentine Onabajo <36359216+BabatundeOnabajo@users.noreply.github.com>
2022-09-29 08:27:58 +0100

    Update send.php

--- send.php
+++ send.php
@@ -2,5 +2,30 @@
 
 # This file controls the actual sending of messages to end users about an interest rate change, with the help of Courier.
 
+# Our script considers the countries in the following order (note that the ordering of this list does not in any way imply a political opinion on the part of the author. This will be used later for the array in which information is stored
+
+/* 1 - United States of America 
+*  2 - Great Britain
+ * 3 - Chile
+ * 4 - South Korea
+ * 5 - Brazil
+ * 6 - Canada
+ * 7 - People's Republic of China
+ *8 - Czech Republic 
+* 9 - Denmark 
+* 10 - Eurozone area
+ * 11 - Hungary
+ * 12 - India
+ * 13 - Israel
+ * 14 - Japan
+ * 15 - Mexico
+ * 16 - New Zealand
+ * 17 - Norway
+ * 18 - Poland
+ * 19 - Russian Federation
+ * 20 - Saudi Arabia
+ * 21 - Switzerland
+ * Turkey */
+
 
 ?>
